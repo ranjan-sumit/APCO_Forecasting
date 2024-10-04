9ca923206fb4eabc69e4335a49d81e503dcfed35
Sumit Ranjan <36401733+ranjan-sumit@users.noreply.github.com>
2024-10-04 14:05:00 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 from prophet import Prophet
 import streamlit as st
 import plotly.graph_objs as go
-
+st.set_page_config(layout="wide")
 # Set up the title of the app
 st.title("APCO Forecasting App")
 st.markdown("---")
